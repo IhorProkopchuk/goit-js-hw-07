da87fd12db670a285160f8c0aade8eb1baa14685
IhorProkopchuk <113351468+IhorProkopchuk@users.noreply.github.com>
2023-02-26 14:32:06 +0200

    Initial commit

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryEl.addEventListener("click", modalOpen);
+
+function createGallery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join(" ");
+}
+
+function modalOpen(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  window.addEventListener("keydown", onEscape);
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  function onEscape(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+console.log(galleryItems);
